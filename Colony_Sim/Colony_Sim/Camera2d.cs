dbf1f646213dd9139252c235d79fc1c7a3dd9e9b
Ryan DuCharme <ryan.ducharme77@gmail.com>
2021-12-19 17:54:48 -0500

    added zoom (scuffed)

--- Colony_Sim/Colony_Sim/Camera2d.cs
+++ Colony_Sim/Colony_Sim/Camera2d.cs
@@ -7,9 +7,10 @@ public static class Camera2d
 {
     public static Matrix Transform { get; set; } = Matrix.Identity; // Matrix Transform
     public static Vector3 Position { get; set; }// Camera Position
+    public static Vector3 Scale { get; set; } // Camera zoom
     public static int Speed { get; set; } = 5;
     public static GraphicsDeviceManager GraphicsDeviceManager { get; set; }
-    
+    public static float Zoom = 1.0f;
     public static Vector2 ScreenToWorldSpace(Vector2 point)
     {
         Matrix invertedMatrix = Matrix.Invert(Transform);
@@ -20,28 +21,39 @@ public static class Camera2d
     {
         KeyboardState key = Keyboard.GetState();
         //move cam left
-        
-        if(Colony_Sim.Input.GetMousePosition().X <= 0)
+
+        if (key.IsKeyDown(Keys.Up))
+        {
+            Zoom+=0.05f;
+            Transform = Matrix.CreateTranslation(Position) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
+        }
+        if (key.IsKeyDown(Keys.Down))
+        {
+            Zoom -= 0.05f;
+            Transform = Matrix.CreateTranslation(Position) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
+        }
+
+        if (Colony_Sim.Input.GetMousePosition().X <= 0)
         {
             Position += new Vector3(Speed, 0, 0);
-            Transform = Matrix.CreateTranslation(Position);
+            Transform = Matrix.CreateTranslation(Position) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
         }
 
         if (Colony_Sim.Input.GetMousePosition().X >= GraphicsDeviceManager.PreferredBackBufferWidth)
         {
             Position -= new Vector3(Speed, 0, 0);
-            Transform = Matrix.CreateTranslation(Position);
+            Transform = Matrix.CreateTranslation(Position) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
         }
         if (Colony_Sim.Input.GetMousePosition().Y <= 0)
         {
             Position += new Vector3(0, Speed, 0);
-            Transform = Matrix.CreateTranslation(Position);
+            Transform = Matrix.CreateTranslation(Position) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
         }
 
         if (Colony_Sim.Input.GetMousePosition().Y >= GraphicsDeviceManager.PreferredBackBufferHeight)
         {
             Position -= new Vector3(0, Speed, 0);
-            Transform = Matrix.CreateTranslation(Position);
+            Transform = Matrix.CreateTranslation(Position) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
         }
     }
 
